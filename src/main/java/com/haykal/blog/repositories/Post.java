555fd57c6dfd432db4dde995f64c432cdcdb20fa
AhmedMohamedAR <ahmed.mohamed@elevate-pay.com>
2025-01-28 18:32:42 +0300

    Implement repositories

---
+++ src/main/java/com/haykal/blog/repositories/Post.java
@@ -0,0 +1,10 @@
+package com.haykal.blog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface Post extends JpaRepository<Post, UUID> {
+}
